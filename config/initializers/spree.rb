24d3c2c11a498c592b6bbdd57a10c57d87a2db69
mostlyClicks <carlos@mostlyclicks.com>
2018-02-20 09:54:51 -0700

    remove bootstrap placeholders

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -22,7 +22,7 @@ Spree.config do |config|
   # Frontend:
 
   # Custom logo for the frontend
-  # config.logo = "logo/solidus_logo.png"
+  config.logo = "Rossignol_Line_RED.png"
 
   # Template to use when rendering layout
   # config.layout = "spree/layouts/spree_application"
