b0a3fafac983ab918f2df93d256ce69a960d0789
mostlyClicks <carlos@mostlyclicks.com>
2018-03-15 15:27:18 -0600

    add spree sizing for skis

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -81,7 +81,8 @@ if Rails.env.production?
       mini:     '48x48>',
       small:    '100x100>',
       product:  '240x240>',
-      large:    '600x600>'
+      large:    '600x600>',
+      skis:     '600x1200>'
     },
 
     path:           '/:class/:id/:style/:basename.:extension',
