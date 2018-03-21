082e38855c6c5fff8663ee1ee2b034056a93273e
mostlyClicks <carlos@mostlyclicks.com>
2018-03-21 10:15:59 -0600

    style breadcrumbs

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -26,4 +26,28 @@ module Spree::BaseHelper
     end
   end
 
+  def taxon_breadcrumbs(taxon, separator = '', breadcrumb_class = 'breadcrumb')
+    return '' if current_page?('/') || taxon.nil?
+
+    crumbs = [[t('spree.home'), spree.root_path]]
+
+    crumbs << [t('spree.products'), products_path]
+    if taxon
+      crumbs += taxon.ancestors.collect { |a| [a.name, spree.nested_taxons_path(a.permalink)] } unless taxon.ancestors.empty?
+      crumbs << [taxon.name, spree.nested_taxons_path(taxon.permalink)]
+    end
+
+    separator = raw(separator)
+
+    items = crumbs.each_with_index.collect do |crumb, i|
+      content_tag(:li, itemprop: 'itemListElement', itemscope: '', itemtype: 'https://schema.org/ListItem') do
+        link_to(crumb.last, itemprop: 'item') do
+          content_tag(:span, crumb.first, itemprop: 'name') + tag('meta', { itemprop: 'position', content: (i + 1).to_s }, false, false)
+        end + (crumb == crumbs.last ? '' : separator)
+      end
+    end
+
+    content_tag(:nav, content_tag(:ol, raw(items.map(&:mb_chars).join), class: breadcrumb_class, itemscope: '', itemtype: 'https://schema.org/BreadcrumbList'), id: 'breadcrumbs', class: 'sixteen columns stuff')
+  end
+
 end
