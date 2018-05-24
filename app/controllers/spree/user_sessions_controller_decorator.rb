37310ead56c70c8e552b9fba438fedda6ba943fe
mostlyclicks <carlos@mostlyclicks.com>
2018-05-24 11:09:56 -0600

    adjust decorator

--- app/controllers/spree/user_sessions_controller_decorator.rb
+++ app/controllers/spree/user_sessions_controller_decorator.rb
@@ -1,5 +1,7 @@
 class Spree::UserSessionsController < Devise::SessionsController
 
+  # before_action :switch_to_brand
+
 
 	def switch_to_brand(brand)
     if brand == "Rossignol"
@@ -21,7 +23,17 @@ class Spree::UserSessionsController < Devise::SessionsController
         format.html do
           flash[:success] = Spree.t(:logged_in_succesfully)
           # redirect_back_or_default(after_sign_in_path_for(spree_current_user))
-          switch_to_brand(current_spree_user.athlete_brand)
+          #switch_to_brand(current_spree_user.athlete_brand)
+
+          brand = current_spree_user.athlete_brand
+          
+          if brand == "Rossignol"
+            redirect_to '/t/rossignol'
+          elsif brand == "Dynastar"
+            redirect_to '/t/dynastar'
+          elsif brand == "Coach"
+            redirect_to '/t/coach'
+          end
 
         end
         format.js { render success_json }
