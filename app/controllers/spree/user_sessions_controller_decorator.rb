2ad155ffef0571f417534b6da0d97905bf12800c
mostlyclicks <carlos@mostlyclicks.com>
2018-05-24 14:28:05 -0600

    add user session controller

--- app/controllers/spree/user_sessions_controller_decorator.rb
+++ app/controllers/spree/user_sessions_controller_decorator.rb
@@ -1,61 +1,61 @@
-class Spree::UserSessionsController < Devise::SessionsController
+# class Spree::UserSessionsController < Devise::SessionsController
 
-  # before_action :switch_to_brand
-
-
-	def switch_to_brand(brand)
-    if brand == "Rossignol"
-      redirect_to 'http://rs11.herokuapp.com/t/rossignol'
-    elsif brand == "Dynastar"
-      redirect_to 'http://rs11.herokuapp.com/t/dynastar'
-    elsif brand == "Coach"
-      redirect_to 'http://rs11.herokuapp.com/t/coach'
-    end
-  end
+#   # before_action :switch_to_brand
 
 
-
-  def create
-  authenticate_spree_user!
-
-    if spree_user_signed_in?
-      respond_to do |format|
-        format.html do
-
-          brand = current_spree_user.athlete_brand
-          puts brand
-          if brand == "Rossignol"
-            redirect_to 'http://rs11.herokuapp.com/t/rossignol'
-          elsif brand == "Dynastar"
-            redirect_to 'http://rs11.herokuapp.com/t/dynastar'
-          elsif brand == "Coach"
-            redirect_to 'http://rs11.herokuapp.com/t/coach'
-          end
-          puts brand + " hello"
+# 	def switch_to_brand(brand)
+#     if brand == "Rossignol"
+#       redirect_to 'http://rs11.herokuapp.com/t/rossignol'
+#     elsif brand == "Dynastar"
+#       redirect_to 'http://rs11.herokuapp.com/t/dynastar'
+#     elsif brand == "Coach"
+#       redirect_to 'http://rs11.herokuapp.com/t/coach'
+#     end
+#   end
 
 
 
-          flash[:success] = Spree.t(:logged_in_succesfully)
-          # redirect_back_or_default(after_sign_in_path_for(spree_current_user))
-          #switch_to_brand(current_spree_user.athlete_brand)
+#   def create
+#   authenticate_spree_user!
+
+#     if spree_user_signed_in?
+#       respond_to do |format|
+#         format.html do
+
+#           brand = current_spree_user.athlete_brand
+#           puts brand
+#           if brand == "Rossignol"
+#             redirect_to 'http://rs11.herokuapp.com/t/rossignol'
+#           elsif brand == "Dynastar"
+#             redirect_to 'http://rs11.herokuapp.com/t/dynastar'
+#           elsif brand == "Coach"
+#             redirect_to 'http://rs11.herokuapp.com/t/coach'
+#           end
+#           puts brand + " hello"
 
 
 
-        end
-        format.js { render success_json }
-      end
-    else
-      respond_to do |format|
-        format.html do
-          flash.now[:error] = t('devise.failure.invalid')
-          render :new
-        end
-        format.js do
-          render json: { error: t('devise.failure.invalid') },
-            status: :unprocessable_entity
-        end
-      end
-    end
-  end
+#           flash[:success] = Spree.t(:logged_in_succesfully)
+#           # redirect_back_or_default(after_sign_in_path_for(spree_current_user))
+#           #switch_to_brand(current_spree_user.athlete_brand)
 
-end
+
+
+#         end
+#         format.js { render success_json }
+#       end
+#     else
+#       respond_to do |format|
+#         format.html do
+#           flash.now[:error] = t('devise.failure.invalid')
+#           render :new
+#         end
+#         format.js do
+#           render json: { error: t('devise.failure.invalid') },
+#             status: :unprocessable_entity
+#         end
+#       end
+#     end
+#   end
+
+# end
